b30bc248e43227396b18f19eef0ffbb83180c4cf
Benalagat <benardkiplagat2000@gmail.com>
2023-12-17 13:24:46 -0800

    updates

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
+    public $name;
+    public function create(){
+        dd('testing');
+    }
     public function render()
     {
         return view('livewire.todo-list');
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,60 +1,6 @@
 <div>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todo App template</title>
-
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body>
-    <div id="head" class="flex border-blue-800 border-t-2">
-        <div class="w-full">
-            <header class="flex bg-white justify-between h-20 border-b border-b-gray-200 items-center px-6">
-                <div id="left-header" class="">
-                </div>
-                <div id="right-header" class="text-gray-800 hover:text-gray-600 space-x-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-7 h-7">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                </div>
-            </header>
-        </div>
-    </div>
-    <div id="content" class="mx-auto" style="max-width:500px;">
-        <div class="container content py-6 mx-auto">
-            <div class="mx-auto">
-                <div id="create-form" class="hover:shadow p-6 bg-white border-blue-500 border-t-2">
-                    <div class="flex ">
-                        <h2 class="font-semibold text-lg text-gray-800 mb-5">Create New Todo</h2>
-                    </div>
-                    <div>
-                        <form>
-                            <div class="mb-6">
-                                <label for="title"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">*
-                                    Todo </label>
-                                <input type="text" id="title" placeholder="Todo.."
-                                    class="bg-gray-100  text-gray-900 text-sm rounded block w-full p-2.5">
-
-                                <span class="text-red-500 text-xs mt-3 block ">Error</span>
-
-                            </div>
-                            <button type="submit"
-                                class="px-4 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600">Create
-                                +</button>
-                            <span class="text-green-500 text-xs">Saved.</span>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div id="content" class="mx-auto" style="max-width:500px;">
+@include('livewire.includes.create-todo-box')
         <div id="search-box" class="flex flex-col items-center px-2 my-4 justify-center">
             <div class="flex justify-center items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -114,9 +60,5 @@
                 <!-- Pagination goes here -->
             </div>
         </div>
-    </div>
-
-</body>
-
-</html>
+</div>
 </div>
